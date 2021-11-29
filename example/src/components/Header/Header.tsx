7e97da3d22c428adc66623dd386b2cfbadac2da5
Pon Cheol Ku (구본철) <bbon@live.com>
2021-11-29 23:55:08 +0900

    Fix minDate, maxDate #17

--- example/src/components/Header/Header.tsx
+++ example/src/components/Header/Header.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
     return (
-        <nav id="header">
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/hello-world">Does not exist page</Link>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <a
-                        href="https://github.com/bbonkr/react-calendar"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Github
-                    </a>
-                </li>
-            </ul>
-        </nav>
+        <React.Fragment>
+            <Helmet titleTemplate="%s - React Calendar example" />
+
+            <nav id="header">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/hello-world">Does not exist page</Link>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a
+                            href="https://github.com/bbonkr/react-calendar"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Github
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </React.Fragment>
     );
 };
